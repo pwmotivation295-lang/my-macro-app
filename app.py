06e06ca958e5b03ed5426e4216153cf62054f1b9
pwmotivation295-lang <pwmotivation295@gmail.com>
2026-06-04 10:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown("""
 # --- 2. USER CONFIGURATION ---
 TARGET_CALORIES = 1600
 TARGET_PROTEIN = 100
-GOOGLE_SHEET_URL = https://docs.google.com/spreadsheets/u/0/d/1Ug8DSMVE7PTJnJfTXxl8XVlHrePmOWTWVF7dJV1pzKQ/htmlview?pli=1
+GOOGLE_SHEET_URL = https://docs.google.com/spreadsheets/d/1Ug8DSMVE7PTJnJfTXxl8XVlHrePmOWTWVF7dJV1pzKQ/edit?usp=drivesdk
 
 # --- 3. DATABASE CONVERSION SYSTEM ---
 def get_csv_url(url):
